cd078ef4d0d788ec77d07d268ef504e9933d79c9
Rails study <souabhgrg713@gmail.com>
2016-04-27 01:10:25 +0530

    dd

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
+
+	def index
+
+	end
+
+	def new
+		@post = Post.new
+
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Your article was successfully created"
+		else
+			render 'new', notice: "Something went wrong"
+		end
+	end
+
+	def show
+	
+
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "successfully updated"
+
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy	
+		@post.destroy
+        redirect_to posts_path
+    end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+
+	end
+
+	def find_post
+	@post = Post.find(params[:id])
+end
+
+end
